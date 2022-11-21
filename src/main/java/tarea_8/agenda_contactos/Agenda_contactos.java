b81b1fe2b8c0d41f51f16c600dea7b50825c9ef8
Martin Moreno <martin.moreno.j04@gmail.com>
2022-11-21 16:42:22 -0500

    primer commit

---
+++ src/main/java/tarea_8/agenda_contactos/Agenda_contactos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package tarea_8.agenda_contactos;
+
+/**
+ *
+ * @author Hexaw
+ */
+public class Agenda_contactos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
